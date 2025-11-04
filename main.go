11187af2ac4dcd56fa4955b5d3dfdd80ca52e62a
Sumit Singh <iamsumitxingh@gmail.com>
2025-11-04 18:12:52 +0530

    added csv file response

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"go-scraper/scraper"
+	"go-scraper/storage"
 )
 
 
@@ -18,9 +19,26 @@ func main() {
 		"https://www.stackoverflow.com",
 		"https://www.reddit.com",
 		"https://www.pinterest.com",
+		"https://www.amazon.com",
+		"https://www.wikipedia.org",
+		"https://www.yahoo.com",
+		"https://www.bing.com",
+		"https://www.duckduckgo.com",
+		"https://www.ask.com",
+		"https://www.ycombinator.com",
+		"https://www.crunchbase.com",
+		"https://www.angellist.com",
+		"https://www.producthunt.com",
+		"https://www.hackernews.com",
+		"https://www.news.ycombinator.com",
 	}
 
 	results := scraper.Run(urls, 10)
+
+	err := storage.SaveToCSV("results.csv", results)
+	if err != nil {
+		fmt.Println("Error saving to CSV:", err)
+	}
 
 	for _, result := range results {
 		fmt.Println(result.URL, result.Title, result.Description)
